ecd980670b64a63ada3cd0d43040360e4738b965
Calvin-braus <75593027+Calvin-braus@users.noreply.github.com>
2024-10-21 12:31:05 +0700

    Update create_event.php

--- admin/create_event.php
+++ admin/create_event.php
@@ -83,7 +83,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['create_event'])) {
                 </ul>
             </div>
             <div class="flex items-center gap-6"> 
-                <a href="logout.php" class="bg-[#7E60BF] text-white px-5 py-2 rounded-full hover:bg-[#CDC1FF]">Log Out</a>
+                <a href="/eventuser/logout.php" class="bg-[#7E60BF] text-white px-5 py-2 rounded-full hover:bg-[#CDC1FF]">Log Out</a>
                 <ion-icon onclick="onToggleMenu(this)" name="menu" class="text-2xl cursor-pointer md:hidden"></ion-icon>
             </div>
         </nav>
